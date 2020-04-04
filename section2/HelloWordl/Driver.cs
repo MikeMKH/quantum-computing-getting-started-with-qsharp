96465b887f95f04df00967718b06aaf227240917
Mike Harris <MikeMKH@users.noreply.github.com>
2020-04-04 09:42:57 -0500

    init as course has it

---
+++ section2/HelloWordl/Driver.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace HelloWordl
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using var qsim = new QuantumSimulator();
+            HelloQ.Run(qsim).Wait();
+        }
+    }
+}
