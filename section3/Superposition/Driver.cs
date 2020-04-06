514e52f9115315e1d9a52c4cdbe1b443c2346aef
Mike Harris <MikeMKH@users.noreply.github.com>
2020-04-06 06:22:02 -0500

    add example

---
+++ section3/Superposition/Driver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Superposition
+{
+    class Driver
+    {
+        static int NUMBER_OF_RUNS = 10_000;
+        static void Main(string[] args)
+        {
+            var ones = 0;
+            
+            using (var qsim = new QuantumSimulator())
+            {
+               for(var i = 0; i <= NUMBER_OF_RUNS; i++)
+               {
+                   var result = Superposition.Run(qsim).Result;
+                   if (result == Result.One) ones++;
+               }
+            }
+            Console.WriteLine("Collapsed State");
+            Console.WriteLine($"\t  One: {(double) ones / NUMBER_OF_RUNS}%");
+            Console.WriteLine($"\t Zero: {(double) (NUMBER_OF_RUNS - ones) / NUMBER_OF_RUNS}%");
+        }
+    }
+}
---
+++ section3/Superposition/Program.qs
@@ -0,0 +1,18 @@
+﻿namespace Superposition {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    
+
+    operation Superposition() : Result {
+        mutable state = Zero;
+        using(qubit = Qubit()){
+            H(qubit);  // put into superposition
+            set state = M(qubit);
+            
+            Reset(qubit);
+        }
+        return state;
+    }
+}
+
