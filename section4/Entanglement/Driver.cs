4e5c9a995234ae3b5bc60d105d54c29e23a63d19
Mike Harris <MikeMKH@users.noreply.github.com>
2020-04-07 06:18:55 -0500

    add example

---
+++ section4/Entanglement/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Entanglement
+{
+    class Driver
+    {
+        static int NUMBER_OF_RUNS = 10_000;
+        static void Main(string[] args)
+        {
+            var ones = 0;
+            var matched = 0;
+            
+            using (var qsim = new QuantumSimulator())
+            {
+                for(var i = 0; i <= NUMBER_OF_RUNS; i++)
+                {
+                    var (r1, r2) = Entanglement.Run(qsim).Result;
+                    
+                    if (r1 == Result.One) ones++;
+                    if (r1 == r2) matched++;
+                } 
+            }
+            Console.WriteLine("Collapsed State");
+            Console.WriteLine($"\t  One: {((decimal) ones / NUMBER_OF_RUNS) * 100.000M}%");
+            Console.WriteLine($"\t Zero: {(((decimal) NUMBER_OF_RUNS - ones) / NUMBER_OF_RUNS)  * 100.000M}%");
+            Console.WriteLine($"\tMatch: {((decimal) matched / NUMBER_OF_RUNS) * 100.000M}%");
+        }
+    }
+}
---
+++ section4/Entanglement/Program.qs
@@ -0,0 +1,20 @@
+﻿namespace Entanglement {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    
+
+    operation Entanglement() : (Result, Result) {
+        mutable s1 = Zero;
+        mutable s2 = Zero;
+        using ((q1, q2) = (Qubit(), Qubit())) {
+            H(q1);
+            CNOT(q1, q2);
+            set s1 = M(q1);
+            set s2 = M(q2);
+            ResetAll([q1, q2]);
+        }
+        return (s1, s2);
+    }
+}
+
